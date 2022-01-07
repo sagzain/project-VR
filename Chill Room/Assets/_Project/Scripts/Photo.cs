c8cfc5e81937acab5ceccb0d924005e7faab72e9
Samuel <samuel.gonzalez3@alu.uclm.es>
2022-01-07 12:35:28 +0100

    #fix photo camera working properly

--- Chill Room/Assets/_Project/Scripts/Photo.cs
+++ Chill Room/Assets/_Project/Scripts/Photo.cs
@@ -1,9 +1,18 @@
+using System.Collections;
 using UnityEngine;
+
 
 public class Photo : MonoBehaviour
 {
     void OnEnable()
     {
-        
+        StartCoroutine(AnimatePhoto());
+    }
+
+    IEnumerator AnimatePhoto()
+    {
+        yield return new WaitForSeconds(.1f);
+
+        transform.SetParent(null);
     }
 }
